1485ef93071c43bf0217388bce3b15fe3ba4a6f3
idankelman <kelmanidan@gmail.com>
2022-02-13 18:36:20 +0200

    Websocket Server Application

---
+++ Server/server/src/main/java/com/example/server/ServerApplication.java
@@ -0,0 +1,20 @@
+package com.example.server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+//==============================================================================
+//                            Server Application
+//==============================================================================
+
+
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
